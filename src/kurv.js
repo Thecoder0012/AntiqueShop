9ec3e582271139ab7ada89ea32507554db299e5e
Mardin <mardineliassi57@gmail.com>
2022-01-17 23:40:53 +0100

    First commit. I created a website in HTML/CSS/JavaScript. Design created in HTML/CSS. Javascript was used for adding products to the cart, for login button and for the contact site, where you can send messages..

---
+++ src/kurv.js
@@ -0,0 +1,39 @@
+/* add to basket */
+let kurv = document.querySelectorAll('.kurv');
+
+gemKurv();
+
+for(let i=0; i < kurv.length; i++){
+
+    kurv[i].addEventListener('click', () => {
+
+        kurvNummer();
+    })  
+}
+
+function gemKurv(){
+    let produkter = localStorage.getItem('kurvNummer');
+
+    if(produkter){
+        document.querySelector('.viskurv span').textContent = produkter;
+
+    }
+}
+
+function kurvNummer(){
+    let produkter = localStorage.getItem('kurvNummer');
+
+    produkter = parseInt(produkter);
+    
+    if(produkter){
+        localStorage.setItem('kurvNummer', produkter + 1);
+        document.querySelector('.viskurv span').textContent = produkter + 1;
+
+    } else {
+
+        localStorage.setItem('kurvNummer', + 1);
+        document.querySelector('.viskurv span').textContent = 1;
+
+    }
+
+}
---
+++ src/login.js
@@ -0,0 +1,20 @@
+/* login button */
+
+function loginButton() {
+    var mail=document.forms["myForm"]["Mail"].value;
+    var kode=document.forms["myForm"]["Kode"].value;
+
+        if(mail == "myemail@gmail.com" && kode == "1234"){
+           document.write('<html> <body> <h2> <center>');
+           document.write("Welcome" + " " + mail);
+          
+           setTimeout(wait, 3000);
+
+           function wait(){
+            window.location.href='home.html'; 
+            document.getElementsByClassName('.login').innerHTML = "Logout";
+           }
+        }else{
+          alert("wrong login");
+        }
+}
